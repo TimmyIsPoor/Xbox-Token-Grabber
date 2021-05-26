99f8c2fb7a2fee0d98ed117c5b01162c092de40a
Timmy <84298281+TimmyIsPoor@users.noreply.github.com>
2021-05-26 10:18:43 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace AM_Token_Grabber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(4);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(27, 25);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.ScrollBarsEnabled = false;
+            this.webBrowser1.Size = new System.Drawing.Size(640, 439);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.TabStop = false;
+            this.webBrowser1.WebBrowserShortcutsEnabled = false;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(640, 439);
+            this.Controls.Add(this.webBrowser1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Xbox Token Grabber";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AM_Token_Grabber
+{
+    public partial class Form1 : Form
+    {
+        string String;
+
+        async void Void()
+        {
+            Stopwatch Stopwatch = new Stopwatch();
+            Stopwatch.Start();
+            for (; ; )
+            {
+                if (Stopwatch.Elapsed.Seconds >= 10)
+                {
+                    Process.Start(Environment.CurrentDirectory + "/Xbox Token Grabber.exe");
+                    Close();
+                }
+                await Task.Delay(1000);
+            }
+        }
+
+        private async void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (webBrowser1.Url.ToString().Contains("oauth20_logout.srf"))
+            {
+                string[] String1 = String.Split(':');
+                webBrowser1.Navigate("https://login.live.com/oauth20_authorize.srf?display=touch&scope=service%3A%3Auser.auth.xboxlive.com%3A%3AMBI_SSL&redirect_uri=https%3A%2F%2Flogin.live.com%2Foauth20_desktop.srf&locale=en&response_type=token&client_id=0000000048093EE3&username=" + String1[0]);
+            }
+            else if (webBrowser1.Url.ToString().Contains("@"))
+            {
+                bool Bool = false;
+                while (!Bool)
+                {
+                    try
+                    {
+                        foreach (HtmlElement HtmlElement in webBrowser1.Document.GetElementsByTagName("div"))
+                        {
+                            if (HtmlElement.GetAttribute("id") == "loginHeader")
+                            {
+                                if (HtmlElement.InnerText == "Enter password")
+                                {
+                                    Bool = true;
+                                    string[] String1 = String.Split(':');
+                                    webBrowser1.Document.GetElementById("i0118").InnerText = String1[1];
+                                    await Task.Delay(500);
+                                    webBrowser1.Document.GetElementById("idSIButton9").InvokeMember("click");
+                                }
+                            }
+                        }
+                    }
+                    catch
+                    {
+
+                    }
+                    await Task.Delay(500);
+                }
+            }
+            else if (webBrowser1.Url.ToString().Contains("access_token"))
+            {
+                try
+                {
+                    string[] String1 = webBrowser1.Url.ToString().Split(new string[] { "access_token=" }, StringSplitOptions.None);
+                    string String2 = String1[1];
+                    string[] String3 = String2.Split('&');
+                    WebClient WebClient = new WebClient();
+                    WebClient.Headers[HttpRequestHeader.ContentType] = "application/json";
+                    string String4 = await WebClient.UploadStringTaskAsync("https://user.auth.xboxlive.com/user/authenticate", "post", "{\"RelyingParty\":\"http://auth.xboxlive.com\",\"TokenType\":\"JWT\",\"Properties\":{\"AuthMethod\":\"RPS\",\"SiteName\":\"user.auth.xboxlive.com\",\"RpsTicket\":\"" + String3[0] + "\"}}");
+                    string[] String5 = String4.Split(new string[] { "Token\":\"" }, StringSplitOptions.None);
+                    string String6 = String5[1];
+                    string[] String7 = String6.Split('"');
+                    StreamWriter StreamWriter = new StreamWriter(Environment.CurrentDirectory + "/Tokens.txt", true);
+                    StreamWriter.WriteLine(String7[0]);
+                    StreamWriter.Close();
+                    if (File.ReadAllLines(Environment.CurrentDirectory + "/Tokens.txt").Count() != File.ReadAllLines(Environment.CurrentDirectory + "/Accounts.txt").Count())
+                    {
+                        Process.Start(Environment.CurrentDirectory + "/Xbox Token Grabber.exe");
+                    }
+                    Close();
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                String = File.ReadLines(Environment.CurrentDirectory + "/Accounts.txt").Skip(File.ReadAllLines(Environment.CurrentDirectory + "/Tokens.txt").Count()).FirstOrDefault();
+                int Integer = File.ReadAllLines(Environment.CurrentDirectory + "/Tokens.txt").Count();
+                Integer++;
+                Text = "Xbox Token Grabber | Account : " + Integer + "/" + File.ReadAllLines(Environment.CurrentDirectory + "/Accounts.txt").Count() + "";
+                Void();
+                webBrowser1.Navigate("https://login.live.com/oauth20_logout.srf");
+            }
+            catch
+            {
+                Close();
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            ServicePointManager.CheckCertificateRevocationList = false;
+            ServicePointManager.DefaultConnectionLimit = 1000;
+            ServicePointManager.Expect100Continue = false;
+            ServicePointManager.UseNagleAlgorithm = false;
+        }
+    }
+}
